2a11f5d4f4a1bd2605c8eef15844354567c33ea4
https://github.com/Sneha-Bhandari/WebTech.git <bsneha6969@gmail.com>
2024-12-21 11:40:40 +0545

    photovideopdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { Formik, Form, Field, ErrorMessage} from 'formik'
-import * as yup from "yup"
-
+import Dayone from "./Files/Dayone";
+import Files from "./Files/files";
 
 function App() {
-  
-  const Forms=[
-    {
-      name:"Username", type:"text", placeholder:"Enter your name"
-    },
-    {
-      name:"Email", type:"email", placeholder:"Enter your email"
-    },
-    {
-      name:"Password", type:"password", placeholder:"Enter your password"
-    },
-    {
-      name:"ConfirmPassword", type:"password", placeholder:"Confirm your password"
-    }
-  ]
 
-
-  const [count, setCount] = useState(0)
-  const Schemas=yup.object().shape({
-    Username: yup.string().required("This is required!!"),
-    Email: yup.string().email("please provide a valid  email").required("please enter your email"),
-    Password: yup.string().max(5,"Password must be at least 5 character!!").required("Password is required"),
-    ConfirmPassword: yup.string().oneOf([yup.ref("Password")]," Confirm password must match the password").required("Confirm password is required")
-  })
   return (
-    
-    <div className='flex justify-center items-center min-h-screen h-lvh bg-gray-500'>
-      <div className='w-full max-w-md p-8'>
-    <Formik className=""initialValues={{
-      Username:"",
-      Email:"",
-      Password:"",
-      ConfirmPassword:""
-
-    }} 
-    validationSchema={Schemas}
-    onSubmit={(values)=>{
-      console.log(values)
-      alert("Submitted Sucessfully!!!")
-    }}
-    >
-      <Form className=' bg-green-200  flex  flex-col  p-2 shadow-lg rounded-xl'>
-        <div>
-          <h1 className='text-black text-center font-bold text-3xl'>Login Form</h1>
-          </div>
-          {
-          Forms.map((val,i)=>{
-            return(
-              <div key={i} className='flex flex-col gap-1 px-2 py-3' >
-                {val.name}
-                <Field className='bg-white border  w-full rounded-md px-2 py-2 outline-none' name= {val.name} type={val.type} placeholder={val.placeholder}/>
-                <ErrorMessage  name={val.name} component={'div'} className='text-red-700'/>
-              </div>
-            )
-          })
-          }
-        <div className='flex items-center justify-center mx-2 my-2'>
-          <button  className='bg-green-600 text-white rounded-lg p-3 w-fit shadow-lg ' type='submit'>Submit</button>
-        </div>
-        
-      </Form>
-    </Formik>
-
-      </div>
-    </div>
-    
-
-      
-    
-  )
+  <div>
+    <Dayone></Dayone>
+    <Files/>
+  </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Files/files.jsx
@@ -0,0 +1,131 @@
+import React from "react";
+
+import { Formik, Form, Field, ErrorMessage } from "formik";
+
+import { MdOutlineBrowserUpdated } from "react-icons/md";
+
+function Files() {
+  return (
+    <div>
+      <div className="flex justify-center items-center min-h-screen h-lvh bg-gray-500">
+        <div className="w-full max-w-md p-8">
+          <Formik
+            className=""
+            initialValues={{
+              Photo: "",
+              Video: "",
+              PDF: "",
+            }}
+          >
+            {({ setFieldValue, values }) => {
+              return (
+                <Form className=" bg-green-200  flex  flex-col  p-2 shadow-lg rounded-xl">
+                  <div>
+                    <h1 className="text-black text-center font-bold text-3xl">
+                      Login Form
+                    </h1>
+                  </div>
+
+{/* image */}
+                  <div className=" flex flex-col">
+                    <h1>Photo</h1>
+                  </div>
+                  <label htmlFor="photo">
+                    {values.Photo.length < 1 ? (
+                      <div className="flex items-center justify-center  bg-gray-200  h-24 w-24">
+                        {" "}
+                        <MdOutlineBrowserUpdated />
+                      </div>
+                    ) : (
+                      <div>
+                         <img src={URL.createObjectURL(values.Photo)} className="h-44 w-44" />
+                      </div>
+                    )}
+                  </label>
+                  <input
+                    onChange={(e) => {
+                      setFieldValue("Photo", e.target.files[0]);
+                      console.log(e.target.files);
+                    }}
+                    type="file"
+                    id="photo"
+                    name="photo"
+                    className="hidden"
+                  />
+
+
+                 {/* video */}
+                  <div className=" flex flex-col">
+                    <h1>Video</h1>
+                  </div>
+                  <label htmlFor="video">
+              {
+                values.Video.length<1?
+              <div className="flex items-center justify-center  bg-gray-200  h-24 w-24">
+                {" "}
+                <MdOutlineBrowserUpdated />
+              </div>
+                :
+                <div>
+                  <img src={URL.createObjectURL(values.Video)} className="h-44 w-44" />
+                </div>
+              }
+            </label>
+                  <input
+                    onChange={(e) => {
+                      setFieldValue("Video", e.target.files[0]);
+                      console.log(e.target.files);
+                    }}
+                    type="file"
+                    id="video"
+                    name="video"
+                    className="hidden"
+                  />
+
+                  {/* pdf  */}
+                  <div className=" flex flex-col">
+                    <h1>PDF</h1>
+                  </div>
+                  <label htmlFor="pdf">
+              {
+                values.PDF.length<1?
+              <div className="flex items-center justify-center  bg-gray-200  h-24 w-24">
+                {" "}
+                <MdOutlineBrowserUpdated />
+              </div>
+                :
+                <div>
+                  <img src={URL.createObjectURL(values.PDF)} className="h-44 w-44" />
+                </div>
+              }
+            </label>
+                  <input
+                    onChange={(e) => {
+                      setFieldValue("PDF", e.target.files[0]);
+                      console.log(e.target.files);
+                    }}
+                    type="file"
+                    id="pdf"
+                    name="pdf"
+                    className="hidden"
+                  />
+
+                  <div className="flex items-center justify-center mx-2 my-2">
+                    <button
+                      className="bg-green-600 text-white rounded-lg p-3 w-fit shadow-lg "
+                      type="submit"
+                    >
+                      Submit
+                    </button>
+                  </div>
+                </Form>
+              );
+            }}
+          </Formik>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Files;
